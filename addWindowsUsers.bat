60982aa728e46b88c289e94f34e7b35bb9a9cc62
Dr. Bryson Payne <31050414+brysonpayne@users.noreply.github.com>
2022-05-30 09:59:45 -0400

    Create addWindowsUsers.bat

---
+++ addWindowsUsers.bat
@@ -0,0 +1,8 @@
+net user ironman Jarvis /add 
+net user ana Password1 /add 
+net user ben P@$$w0rd! /add 
+net user carol CaptainMarvel /add 
+net user clark superman20 /add 
+net user kara SuperGirl7! /add 
+net user peter SpidermanRulez:) /add 
+net user bryson Don'tyouwishyourpasswordwastoughlike mine! /add 
